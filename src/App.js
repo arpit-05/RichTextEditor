a41202dc01754c87bf841296b0811094ef1c3034
Priyanka <priyanka.chaudhary@infogain.com>
2019-05-28 18:02:09 +0530

    Some Changes in CSS by Priyanka

--- src/App.js
+++ src/App.js
@@ -1,45 +1,40 @@
-import React from 'react';
-import {DUMMY} from './shared/DUMMY'
-import Text from './components/Text'
-import Attachments from './components/Attachments'
-import Field from './components/Field'
-import Action from './components/Action'
-import Footer from './components/Footer'
+import React from "react";
+import { DUMMY } from "./shared/DUMMY";
+import Text from "./components/Text";
+import Attachments from "./components/Attachments";
+import Field from "./components/Field";
+import Action from "./components/Action";
+import Footer from "./components/Footer";
 import AddPhoto from "@material-ui/icons/AddPhotoAlternate";
 
-import './App.css';
-class App extends React.Component{
-  constructor(props){
+import "./App.css";
+class App extends React.Component {
+  constructor(props) {
     super(props);
-    this.state={
-      dummy:DUMMY
-    }
+    this.state = {
+      dummy: DUMMY
+    };
   }
-  render(){
-    return(
-    <div className="container">
-     <div className="row ">
-     <div className="outer">
-     <Text className="text" dummy={this.state.dummy}/>
-     <div className="Wrapper">
-     
-      <Attachments dummy={this.state.dummy}/>
-      <Field dummy={this.state.dummy}></Field>
-      <Action dummy={this.state.dummy}/>
-      <Footer dummy={this.state.dummy}/>
-     
-     </div>
-     
-     
-     </div>
-     
-     </div>
-    </div>
+  render() {
+    return (
+      <div className="container">
+        <div className="row ">
+          <div className="outer">
+            <Text className="text" dummy={this.state.dummy} />
+            <div className="Wrapper">
+              <Attachments dummy={this.state.dummy} />
+              <Field dummy={this.state.dummy} />
+              <Action dummy={this.state.dummy} />
+              <Footer dummy={this.state.dummy} />
+            </div>
+          </div>
+        </div>
+      </div>
 
-  //   <div class="box">
-  //   <div></div>
-  // </div>
-    )
+      //   <div class="box">
+      //   <div></div>
+      // </div>
+    );
   }
 }
 
